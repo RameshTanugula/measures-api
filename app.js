fef56124044f33b4a7919181095f9093f7967cc6
RameshTanugula <gnitramesh@gmail.com>
2022-09-14 15:30:54 +0530

    login changes added

--- app.js
+++ app.js
@@ -6,48 +6,65 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 var corsOptions = {
-    origin: "*"
-  };
-  app.use(cors(corsOptions));
+  origin: "*"
+};
+app.use(cors(corsOptions));
 // Handling '/' request
 app.delete('/delete', (req, res) => {
-        let query = `delete from measures;`;
-     
-      sql.query(query, (err, result) => {
-        if (err) {
-        res.send({error: `Something went wrong ${err}`});
-        } else{
-        res.send({res: result});
-        }
-      });
-    });
-app.get('/getdata', (req, res) => {
-    let query = `select * from measures;`;
+  let query = `delete from measures;`;
+
   sql.query(query, (err, result) => {
     if (err) {
-    res.send({error: `Something went wrong ${err}`});
-    } else{
-    res.send({res: result});
+      res.send({ error: `Something went wrong ${err}` });
+    } else {
+      res.send({ res: result });
+    }
+  });
+});
+app.get('/getdata', (req, res) => {
+  let query = `select * from measures;`;
+  sql.query(query, (err, result) => {
+    if (err) {
+      res.send({ error: `Something went wrong ${err}` });
+    } else {
+      res.send({ res: result });
     }
   });
 });
 app.post('/save', (req, res) => {
-const calulatedValues = commonService.caluculateValues(req.body.data);
-var preparedData = commonService.prepareData(calulatedValues);
-    let query = `delete from measures; 
+  const calulatedValues = commonService.caluculateValues(req.body.saveData);
+  var preparedData = commonService.prepareData(calulatedValues);
+  let query = `delete from measures; 
     ALTER TABLE measures AUTO_INCREMENT = 1;
     INSERT INTO measures (title, years)
-    VALUES `+preparedData+`;`;
- 
+    VALUES `+ preparedData + `;`;
+
   sql.query(query, (err, result) => {
     if (err) {
-    res.send({error: `Something went wrong ${err}`});
-    } else{
-    res.send({res: 'Data Saved Successfully'});
+      res.send({ error: `Something went wrong ${err}` });
+    } else {
+      res.send({ res: 'Data Saved Successfully' });
     }
   });
-    
+
+});
+// login
+app.post('/login', (req, res) => {
+  const userName = req.body.loginData?.username;
+  const password = req.body.loginData?.password;
+  if (userName && password) {
+    let query = `select * from users where userName ='${userName}' and password='${password}'`;
+    sql.query(query, (err, result) => {
+      if (result?.length > 0) {
+        res.send({ message: 'success' });
+      } else {
+        res.send({ message: `Something went wrong ${err}` });
+      }
+    });
+  } else {
+    res.send({ message: 'Login falied' })
+  }
 });
 app.listen(3000, () => {
-    console.log('server listening on port 3000');
+  console.log('server listening on port 3000');
 });
--- config/db_table.sql
+++ config/db_table.sql
@@ -1,7 +1,15 @@
-CREATE DATABASE test;
+CREATE DATABASE measures_db;
 
 CREATE TABLE IF NOT EXISTS measures (
     id int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
     title varchar(255) NOT NULL,
     years varchar(255)
   );
+
+  CREATE TABLE IF NOT EXISTS users (
+    id int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    userName varchar(255) NOT NULL,
+    password varchar(255)
+  );
+  
+  insert into users (userName, password) values('Admin', 'Admin@123');
